ea1f69d1d3e30b68b7676aff90d5b7492595b4c1
mararap <mzingerle@gmail.com>
2025-05-07 10:11:42 +0200

    Added libft

---
+++ 42_libft/ft_strlcpy.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marapovi <marapovi@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 13:20:13 by marapovi          #+#    #+#             */
+/*   Updated: 2025/05/02 13:20:19 by marapovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <bsd/string.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy (char	*dest, char	*src, size_t	size)
+{
+	size_t	i;
+
+	i = 0;
+
+	if (size == 0)
+		return ft_strlen(src);
+	while ((i < (size-1)) && (src[i]))
+			{
+				(dest[i]) = (src[i]);
+					i++;
+			}
+			dest[i] = '\0';
+	return ft_strlen(src);
+}
+
+int	main(void)
+{
+	char dest[25] = "nowtestme";
+	char *src = "testmetestme";
+	size_t size = 4;
+
+	printf("%zu\n", strlcpy(dest, src, size));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, size));
+	printf("%s\n", dest);
+	return(0);
+}
---
+++ 42_libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marapovi <marapovi@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 11:19:25 by marapovi          #+#    #+#             */
+/*   Updated: 2025/04/25 11:19:30 by marapovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *str);
+
+#endif // LIBFT_H
