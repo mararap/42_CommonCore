6779dc3a5c5585680ed91e859b8c7813b0889662
Maria Arapovic <marapovi@student.42vienna.com>
2025-05-15 15:05:09 +0200

    updated

--- 42_libft/libft.h
+++ 42_libft/libft.h
@@ -32,5 +32,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif // LIBFT_H
