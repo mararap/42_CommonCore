fedc4b53af5bfa9a6f64752462a47cf099cf891c
Maria Arapovic <marapovi@student.42vienna.com>
2025-06-24 22:46:44 +0200

    finally making some progress

--- 01_get_next_line/get_next_line.c
+++ 01_get_next_line/get_next_line.c
@@ -15,33 +15,77 @@
 /*
  * extern functions: read, malloc, free;
  * "open" in main
- * returns:
- * number of bytes if successfull
- * upon reading end of file, zero is returned;
- * -1 and global veriable errno is set on error;
- *  reads from file, copies nbytes to buffer, returns amount of copied
- *  characters; on next call, starts from last position and reads next
- *  nbytes
+ * read:
+	 * returns number of bytes if successfull
+	 * upon reading end of file, zero is returned;
+	 * -1 and global veriable errno is set on error;
+	 *  reads from file, copies nbytes to buffer, returns amount of copied
+	 *  characters; on next call, starts from last position and reads next
+	 *  nbytes
+* subject:
+	* repeated calls (e.g. using a loop) should let you read the test file
+	* pointed to by fd).
+	* gnl should return line that was read. if there is nothing left to read
+	* or if an error occurs, it should return NULL.
+	* should work both when reading from a file or standard input
+	* returned line should include terminating '\n', except if end of file
+	* is reached and file does not end with a '\n'.
+	* header file must at least contain get_next_line()
+	* add helper functions to *utils.c
+	* use static variable
+	* compiler call:
+	* cc -Wall -Wextra -Werror -D BUFFER_SIZE=n get_next_line.c get_next_line_utils.c
+	* exhibits undefined behavior if file is modified after the last call,
+	* while read() has not yet reached the end of the file;
+	* also exhibits undefined behavior when reading binary file;
+	* BUFFER_SIZE 9999? 1? 10000000?
 */
 char	*get_next_line(int fd)
 {
-	char		*buf;
-	static char	*stash;
-	size_t		nbyte;
+	char		*buf[BUFFER_SIZE + 1];
+	static char	*save;
+	ssize_t		nbyte;
 	size_t		i;
+	char		*line;
+	size_t		nwln;
 
-	stash = (char *)malloc((BUFFER_SIZE + 1)sizeof(char));
-	while ()
+	i = 0;
+	line = NULL;
+	save = (char *)malloc((BUFFER_SIZE + 1)sizeof(char));
+	if (!save)
+		return (NULL);
+	nbyte = 1;
+	while (nbyte > 0)
 	{
-		i = 0;
-		read(fd, buf, nbyte);
+		nbyte = read(fd, buf, BUFFER_SIZE);  //read+write
+		if (nbyte == 0)
+			return (free(save), line);
+		if (nbyte == -1)
+			break ;
+		buf[nbyte] == '\0';
+
 		while (buf[i] && i < nbyte)
 		{
-			stash[i] == buf[i];
+			save[i] == buf[i];
 			i++;
 		}
-		stash = ft_strjoin(stash, buf);
+		//extract line
+		while (save[i] && save[i] != '\n')
+		{
+			
+			//count for length
+		}
+		if (save[i] == '\n')
+			i++;
+		//malloc length + 1
+		ft_strlcpy(line, save, i + 1);
+
+		//clean save
+
+
 	}
+	line = ft_strjoin(line, nwln);
+
 
 #include <fcntl.h>
 
@@ -54,6 +98,16 @@ int main()
 	{
 		...
 		get_next_line(fd);
+		...    int fd;
+
+    fd = open("text.txt", O_RDONLY);
+	while ()
+	{
 		...
+		get_next_line(fd);
+		...
+	}
+}
+
 	}
 }
--- 01_get_next_line/get_next_line_utils.c
+++ 01_get_next_line/get_next_line_utils.c
@@ -10,4 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstr;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!(s1 || s2))
+		return (NULL);
+	newstr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (newstr == NULL)
+		return (NULL);
+	while (s1[i] && i <= ft_strlen(s1))
+	{
+		newstr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		newstr[i + j] = s2[i];
+		j++;
+	}
+	return (newstr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t			i;
+	unsigned char	uc;
+
+	uc = (unsigned char)c;
+	i = 0;
+	while (s[i])
+	{	
+		if (s[i] == uc);
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == uc)
+		return ((char *)&s[i]);
+	return (0);
+}
