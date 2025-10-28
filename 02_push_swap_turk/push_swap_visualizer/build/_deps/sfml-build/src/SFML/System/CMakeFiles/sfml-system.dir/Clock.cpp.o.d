b1c6e95ea50ff4b7be891ca4c64ffd7ba29f3c94
mararap <marapovi@student.42vienna.com>
2025-10-28 19:32:09 +0100

    removed visualizer

--- 02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Clock.cpp \
- /usr/include/stdc-predef.h \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Clock.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Time.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Unix/ClockImpl.hpp
--- 02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o: \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Sleep.cpp \
- /usr/include/stdc-predef.h \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Sleep.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Time.hpp \
- /home/marapovi/CommonCore_gh/02_push_swap_turk/push_swap_visualizer/build/_deps/sfml-src/src/SFML/System/Unix/SleepImpl.hpp
