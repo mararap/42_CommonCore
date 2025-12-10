658d111ea5d767eb7dc277742af95a20ad646a59
mararap <marapovi@student.42vienna.com>
2025-12-10 22:37:58 +0100

    Ex03: getting closer to understanding n_queens, simplified code a little bit

--- 03_Exam_preparation/n_queens/mynqueens.c
+++ 03_Exam_preparation/n_queens/mynqueens.c
@@ -1,26 +1,19 @@
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int	*solution;
-int	N;
-/*
-unsigned int	my_abs(int num)
-{
-	if (num < 0)
-		num *= -1;
-	return (num);
-}*/
+int	*g_solution;
+int	g_n;
 
 void	print_solution()
 {
 	int i;
 
 	i = 0;
-	while (i < N)
+	while (i < g_n)
 	{
-		fprintf(stdout, "%d", solution[i]);
-		if (i + 1 < N)
+		fprintf(stdout, "%d", g_solution[i]);
+		if (i + 1 < g_n)
 			fprintf(stdout, " ");
 		i++;
 	}
@@ -32,42 +25,37 @@ int	is_safe(int x, int y)
 	int x_temp = 0;	
 	int value = 0;
 	
-	while (x_temp < x)	// for every column-index until we reach the current(?)
+	while (x_temp < x)	                // for every column-index until we reach the current(?)
 	{
-		value = solution[x_temp];	// value = value of solution[0], [1], [2] ...
+		value = g_solution[x_temp];     // value = value of solution[0], [1], [2] ...
 		if (value == y)
 			return (0);
 		if ((value - x_temp) == (y - x))
 			return (0);
 		if ((value + x_temp) == (y + x))
 			return (0);
-//		if (my_abs(x_temp - x) == my_abs(y_temp - y))
-//			return (0);
 		x_temp++;
 	}
 	return (1);
 }
 
-void	solve(int x) 				// function called with 0
-{									// therefor first line-idx = 0
+void	solve(int x)     				// function called with 0
+{				    					// therefor first line-idx = 0
 	int y = 0; 
 	
-	if (x == N)						// once we reach the final line-idx
+	if (x == g_n)			    		// once we reach the final line-idx
 	{
-		if (N < 4)
-			fprintf(stdout, "\n");
-		else
-			print_solution();			// we call print function
+		print_solution();   			// call print function
 		return ;
 	}
-	while (y < N)					// until we reach last col-idx
-	{
-		if (is_safe(x, y))			// if is_save returns 1
-		{							// aka position at row-idx X col-idx is save,
-			solution[x] = y;		// fill current row-position with value of col@col-idx?
-			solve(x + 1);			// call solve-function recursivly for
-		}							// next row as long as is_save keeps
-		y++;						// returning 1
+	while (y < g_n)		    			// until we reach last col-idx
+    {	
+		if (is_safe(x, y))		    	// if is_save returns 1
+		{					    		// aka position at row-idx X col-idx is save,
+			g_solution[x] = y;		    // fill current row-position with value of col@col-idx?
+			solve(x + 1);			    // call solve-function recursivly for
+		}					    		// next row as long as is_save keeps
+		y++;					    	// returning 1
 	}
 }
 
@@ -75,15 +63,13 @@ int	main(int ac, char **av)
 {
 	if (ac != 2 || !av[0][1]) 			// check args
 		return (1);
-	N = atoi(av[1]);					// set number of queens/rows/columns to av[1]
-	if (N < 0) 
+	g_n = atoi(av[1]);					// set number of queens/rows/columns to av[1]
+	if (g_n < 0) 
 		return (0);
-	if (N < 4)
-		return (fprintf(stdout, "\n"), 0);
-	solution = malloc(sizeof(int) * N);	// allocate for int *solution
-	if (!solution)
+	g_solution = malloc(sizeof(int) * g_n);	// allocate for int *solution
+	if (!g_solution)
 		return (1);
 	solve(0);							// call function
-	free(solution);						// free
+	free(g_solution);						// free
 	return (0);							// return
 }
